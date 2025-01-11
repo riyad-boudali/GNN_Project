211d0697ebb4f9df24630cfdeda1ead5bcd4aba2
Riyad <r.boudali@esi-sba.dz>
2025-01-11 23:01:22 +0100

    feat: Final model

--- dataset_featurizer.py
+++ dataset_featurizer.py
@@ -2,53 +2,66 @@ import pandas as pd
 import torch
 import torch_geometric
 from torch_geometric.data import Dataset
-import numpy as np
+import numpy as np 
 import os
 from tqdm import tqdm
 import deepchem as dc
+from rdkit import Chem 
 
 print(f"Torch version: {torch.__version__}")
 print(f"Cuda available: {torch.cuda.is_available()}")
 print(f"Torch geometric version: {torch_geometric.__version__}")
 
-
 class MoleculeDataset(Dataset):
-    def __init__(self, root, filename, transform=None, pre_transform=None):
+    def __init__(self, root, filename, test=False, transform=None, pre_transform=None):
         """
         root = Where the dataset should be stored. This folder is split
-        into raw_dir (downloaded dataset) and processed_dir (processed data).
+        into raw_dir (downloaded dataset) and processed_dir (processed data). 
         """
+        self.test = test
         self.filename = filename
         super(MoleculeDataset, self).__init__(root, transform, pre_transform)
-
+        
     @property
     def raw_file_names(self):
-        """If this file exists in raw_dir, the download is not triggered.
-        (The download func. is not implemented here)
+        """ If this file exists in raw_dir, the download is not triggered.
+            (The download func. is not implemented here)  
         """
         return self.filename
 
     @property
     def processed_file_names(self):
-        """If these files are found in raw_dir, processing is skipped"""
-        self.data = pd.read_csv(self.raw_paths[0])
-        self.data.index = self.data["index"]
-        return [f"data_{i}.pt" for i in list(self.data.index)]
+        """ If these files are found in raw_dir, processing is skipped"""
+        self.data = pd.read_csv(self.raw_paths[0]).reset_index()
+
+        if self.test:
+            return [f'data_test_{i}.pt' for i in list(self.data.index)]
+        else:
+            return [f'data_{i}.pt' for i in list(self.data.index)]
+        
 
     def download(self):
         pass
 
     def process(self):
-        self.data = pd.read_csv(self.raw_paths[0])
-        self.data.index = self.data["index"]
+        self.data = pd.read_csv(self.raw_paths[0]).reset_index()
         featurizer = dc.feat.MolGraphConvFeaturizer(use_edges=True)
-        for index, mol in tqdm(self.data.iterrows(), total=self.data.shape[0]):
+        for index, row in tqdm(self.data.iterrows(), total=self.data.shape[0]):
             # Featurize molecule
-            f = featurizer.featurize(mol["smiles"])
-            data = f[0].to_pyg_graph()
-            data.y = self._get_label(mol["HIV_active"])
-            data.smiles = mol["smiles"]
-            torch.save(data, os.path.join(self.processed_dir, f"data_{index}.pt"))
+            mol = Chem.MolFromSmiles(row["smiles"])
+            f = featurizer._featurize(mol)
+            data = f.to_pyg_graph()
+            data.y = self._get_label(row["HIV_active"])
+            data.smiles = row["smiles"]
+            if self.test:
+                torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'data_test_{index}.pt'))
+            else:
+                torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'data_{index}.pt'))
+            
 
     def _get_label(self, label):
         label = np.asarray([label])
@@ -58,8 +71,13 @@ class MoleculeDataset(Dataset):
         return self.data.shape[0]
 
     def get(self, idx):
-        """- Equivalent to __getitem__ in pytorch
-        - Is not needed for PyG's InMemoryDataset
+        """ - Equivalent to __getitem__ in pytorch
+            - Is not needed for PyG's InMemoryDataset
         """
-        data = torch.load(os.path.join(self.processed_dir, f"data_{idx}.pt"))
+        if self.test:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                 f'data_test_{idx}.pt'))
+        else:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                 f'data_{idx}.pt'))        
         return data
--- model.py
+++ model.py
@@ -1,67 +1,86 @@
 import torch
 import torch.nn.functional as F 
-from torch.nn import Sequential, Linear, BatchNorm1d, ReLU
-from torch_geometric.nn import TransformerConv, GATConv, TopKPooling, BatchNorm
+from torch.nn import Linear, BatchNorm1d, ModuleList
+from torch_geometric.nn import TransformerConv, TopKPooling 
 from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
-from torch_geometric.nn.conv.x_conv import XConv
 torch.manual_seed(42)
 
 class GNN(torch.nn.Module):
-    def __init__(self, feature_size):
+    def __init__(self, feature_size, model_params):
         super(GNN, self).__init__()
-        num_classes = 2
-        embedding_size = 1024
-    
-        # GNN layers
-        self.conv1 = GATConv(feature_size, embedding_size, heads=3, dropout=0.3)
-        self.head_transform1 = Linear(embedding_size*3, embedding_size)
-        self.pool1 = TopKPooling(embedding_size, ratio=0.8)
-        self.conv2 = GATConv(embedding_size, embedding_size, heads=3, dropout=0.3)
-        self.head_transform2 = Linear(embedding_size*3, embedding_size)
-        self.pool2 = TopKPooling(embedding_size, ratio=0.5)
-        self.conv3 = GATConv(embedding_size, embedding_size, heads=3, dropout=0.3)
-        self.head_transform3 = Linear(embedding_size*3, embedding_size)
-        self.pool3 = TopKPooling(embedding_size, ratio=0.2)
+        embedding_size = model_params["model_embedding_size"]
+        n_heads = model_params["model_attention_heads"]
+        self.n_layers = model_params["model_layers"]
+        dropout_rate = model_params["model_dropout_rate"]
+        top_k_ratio = model_params["model_top_k_ratio"]
+        self.top_k_every_n = model_params["model_top_k_every_n"]
+        dense_neurons = model_params["model_dense_neurons"]
+        edge_dim = model_params["model_edge_dim"]
+
+        self.conv_layers = ModuleList([])
+        self.transf_layers = ModuleList([])
+        self.pooling_layers = ModuleList([])
+        self.bn_layers = ModuleList([])
+
+        # Transformation layer
+        self.conv1 = TransformerConv(feature_size, 
+                                    embedding_size, 
+                                    heads=n_heads, 
+                                    dropout=dropout_rate,
+                                    edge_dim=edge_dim,
+                                    beta=True) 
+
+        self.transf1 = Linear(embedding_size*n_heads, embedding_size)
+        self.bn1 = BatchNorm1d(embedding_size)
+
+        # Other layers
+        for i in range(self.n_layers):
+            self.conv_layers.append(TransformerConv(embedding_size, 
+                                                    embedding_size, 
+                                                    heads=n_heads, 
+                                                    dropout=dropout_rate,
+                                                    edge_dim=edge_dim,
+                                                    beta=True))
+
+            self.transf_layers.append(Linear(embedding_size*n_heads, embedding_size))
+            self.bn_layers.append(BatchNorm1d(embedding_size))
+            if i % self.top_k_every_n == 0:
+                self.pooling_layers.append(TopKPooling(embedding_size, ratio=top_k_ratio))
+            
 
         # Linear layers
-        self.linear1 = Linear(embedding_size*2, 1024)
-        self.linear2 = Linear(1024, num_classes)  
+        self.linear1 = Linear(embedding_size*2, dense_neurons)
+        self.linear2 = Linear(dense_neurons, int(dense_neurons/2))  
+        self.linear3 = Linear(int(dense_neurons/2), 1)  
 
     def forward(self, x, edge_attr, edge_index, batch_index):
-        # First block
-        x = self.conv1(x, edge_index)
-        x = self.head_transform1(x)
-        
-        x, edge_index, edge_attr, batch_index, _, _ = self.pool1(x, 
-                                                        edge_index, 
-                                                        None, 
-                                                        batch_index)
-        x1 = torch.cat([gmp(x, batch_index), gap(x, batch_index)], dim=1)
+        # Initial transformation
+        x = self.conv1(x, edge_index, edge_attr)
+        x = torch.relu(self.transf1(x))
+        x = self.bn1(x)
 
-        # Second block
-        x = self.conv2(x, edge_index)
-        x = self.head_transform2(x)
-        x, edge_index, edge_attr, batch_index, _, _ = self.pool2(x, 
-                                                        edge_index, 
-                                                        None, 
-                                                        batch_index)
-        x2 = torch.cat([gmp(x, batch_index), gap(x, batch_index)], dim=1)
+        # Holds the intermediate graph representations
+        global_representation = []
 
-        # Third block
-        x = self.conv3(x, edge_index)
-        x = self.head_transform3(x)
-        x, edge_index, edge_attr, batch_index, _, _ = self.pool3(x, 
-                                                        edge_index, 
-                                                        None, 
-                                                        batch_index)
-        x3 = torch.cat([gmp(x, batch_index), gap(x, batch_index)], dim=1)
-
-        # Concat pooled vectors
-        x = x1 + x2 + x3
+        for i in range(self.n_layers):
+            x = self.conv_layers[i](x, edge_index, edge_attr)
+            x = torch.relu(self.transf_layers[i](x))
+            x = self.bn_layers[i](x)
+            # Always aggregate last layer
+            if i % self.top_k_every_n == 0 or i == self.n_layers:
+                x , edge_index, edge_attr, batch_index, _, _ = self.pooling_layers[int(i/self.top_k_every_n)](
+                    x, edge_index, edge_attr, batch_index
+                    )
+                # Add current representation
+                global_representation.append(torch.cat([gmp(x, batch_index), gap(x, batch_index)], dim=1))
+    
+        x = sum(global_representation)
 
         # Output block
-        x = self.linear1(x).relu()
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.linear2(x)
+        x = torch.relu(self.linear1(x))
+        x = F.dropout(x, p=0.8, training=self.training)
+        x = torch.relu(self.linear2(x))
+        x = F.dropout(x, p=0.8, training=self.training)
+        x = self.linear3(x)
 
         return x
